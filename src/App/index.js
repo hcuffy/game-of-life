21e7d041fa1e6b17b765a14e0a0f83f729e25b63
Henry Cuffy <hcuffy_5@hotmail.com>
2018-05-20 13:54:39 +0200

    Initial Commit

---
+++ src/App/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
