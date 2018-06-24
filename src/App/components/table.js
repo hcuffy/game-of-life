4655609cc4f27197ec8335a19fdede7b63335eb2
Henry Cuffy <hcuffy_5@hotmail.com>
2018-06-24 17:19:16 +0200

    Prettier

--- src/App/components/table.js
+++ src/App/components/table.js
@@ -2,11 +2,10 @@ import React from 'react';
 import '../style.css';
 import Cell from './cell';
 
-<<<<<<< HEAD
-const Table = ({ board }) => {
+const Table = ({ board, size }) => {
 	const rows = [];
-	const height = board.length;
-	const width = board.length;
+	const height = size;
+	const width = size;
 
 	for (let i = 0; i < height; i++) {
 		let row = [];
@@ -22,36 +21,5 @@ const Table = ({ board }) => {
 		</table>
 	);
 };
-=======
-const Table = ({board, size}) => {
-
-  const rows = [];
-  const height = size;
-  const width = size;
-
-  for (let i = 0; i < height; i++) {
-    let row = [];
-    for (let j = 0; j < width; j++) {
-      row.push(<Cell check = {
-        board[i][j]
-      }
-      key = {
-        'cell' + i + j
-      } />)
-    }
-    rows.push(
-      <tr key={'row' + i}>{row}</tr>
-    )
-  }
-
-  return (
-    <table className="theBoard">
-      <tbody>
-        {rows}
-      </tbody>
-    </table>
-  )
-}
->>>>>>> 61fc038f792e9721b6f31fb48606d0054e6bca9b
 
 export default Table;
